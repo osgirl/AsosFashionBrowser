432f70fe149ed1b489d5a3bbc8f1d4265ada2856
HoraceBG <horaceb1986@gmail.com>
2015-07-27 19:53:46 +0100

    #7 WIP navigation drawer. Displays a drawer, toggle and the categories. Need to filter by gender now.

--- app/src/main/java/com/horaceb/asosfashionbrowser/ui/activity/HomeActivity.java
+++ app/src/main/java/com/horaceb/asosfashionbrowser/ui/activity/HomeActivity.java
@@ -1,15 +1,26 @@
 package com.horaceb.asosfashionbrowser.ui.activity;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import com.horaceb.asosfashionbrowser.R;
 import com.horaceb.asosfashionbrowser.data.provider.FashionBrowserContract;
 import com.horaceb.asosfashionbrowser.service.CategoryIntentService;
 import com.horaceb.asosfashionbrowser.service.CategorySyncReceiver;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 import static com.horaceb.asosfashionbrowser.IntentActions.ERROR;
@@ -23,7 +34,18 @@ import static com.horaceb.asosfashionbrowser.IntentActions.SUCCESSFUL;
  * <p/>
  * Created by HoraceBG on 23/07/15.
  */
-public class HomeActivity extends AppCompatActivity implements CategorySyncReceiver.Receiver {
+public class HomeActivity extends AppCompatActivity implements CategorySyncReceiver.Receiver, LoaderManager.LoaderCallbacks<Cursor> {
+
+    @Bind(R.id.home_drawer_layout)
+    DrawerLayout drawerLayout;
+
+    @Bind(R.id.home_toolbar)
+    Toolbar toolbar;
+
+    @Bind(R.id.left_nav_drawer_list)
+    ListView navigationDrawerList;
+
+    private SimpleCursorAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +53,40 @@ public class HomeActivity extends AppCompatActivity implements CategorySyncRecei
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
 
+        // Load the categories in the background for display in the navigation drawer
         CategorySyncReceiver receiver = new CategorySyncReceiver(new Handler());
         receiver.setReceiver(this);
         Intent intent = new Intent(Intent.ACTION_SYNC, FashionBrowserContract.CATEGORY_URI, this, CategoryIntentService.class);
         intent.putExtra(RECEIVER, receiver);
         startService(intent);
+
+        setSupportActionBar(toolbar);
+        setupNavigationDrawer();
+
+        // Prepare to query the provider
+        getLoaderManager().initLoader(0, null, this);
+
+    }
+
+
+    private void setupNavigationDrawer() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawerLayout.setDrawerListener(toggle);
+        getDelegate().getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getDelegate().getSupportActionBar().setHomeButtonEnabled(true);
+        toggle.syncState();
+
+        adapter = new SimpleCursorAdapter(
+                this, R.layout.list_item_category,
+                null,
+                new String[]{FashionBrowserContract.CategoryColumns.NAME},
+                new int[]{R.id.category_name},
+                0);
+
+        navigationDrawerList.setAdapter(adapter);
     }
 
     @Override
@@ -51,5 +102,21 @@ public class HomeActivity extends AppCompatActivity implements CategorySyncRecei
                 // Display an error to the user
                 break;
         }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Uri categoryUri = FashionBrowserContract.CATEGORY_URI;
+        return new CursorLoader(this, categoryUri, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        adapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader loader) {
+
     }
 }
